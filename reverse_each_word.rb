2ff762d9e6b5ddb210c54fcd6682a440353aa086
Fabian Fabro <fabrofabian@gmail.com>
2019-04-26 03:30:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  split_string = string.split(" ")
+  reversed = []
+
+  split_string.collect do |word|
+    reversed << word.reverse
+  end
+  reversed.join(" ")
+end
