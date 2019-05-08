0920095369f06705765f4e0c37571e9ad6fe9f2f
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 22:18:11 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(sentence1)
     new_array = sentence1.split(",")
     new_array.each do |word|
       word.reverse
-      new_array << word
     end
-    new_array
+    new_array(word)
 end
