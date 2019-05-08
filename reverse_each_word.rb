59f6d51f34fa71902c382e53fe35d477871b340a
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 23:17:30 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,13 @@ def reverse_each_word(sentence1)
     new_array.each do |word|
       word.reverse!
     end
-    new_array.join("")
+    new_array.join(" ")
+end
+
+def reverse_each_word(sentence1)
+    new_array = sentence1.split
+    new_array.collect do |word|
+      word.reverse!
+    end
+    new_array.join(" ")
 end
